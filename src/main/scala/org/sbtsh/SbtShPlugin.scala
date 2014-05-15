2700d70107c2790f9e13e9d31d14fcef5a7975ac
Vincent PERICART <v-pericart@m3.com>
2014-05-15 15:07:01 +0900

    fail when the process return status is != 0

--- src/main/scala/org/sbtsh/SbtShPlugin.scala
+++ src/main/scala/org/sbtsh/SbtShPlugin.scala
@@ -6,10 +6,12 @@ object SbtShPlugin extends Plugin {
 
   override lazy val settings = Seq(Keys.commands += shCommand)
 
-  def shCommand = Command.args("sh", "<shell command>") { (state, args) =>
-    val ret = args.mkString(" ") !
-
-    state
+  def shCommand = Command.args("sh", "<shell command>") {
+    (state, args) =>
+      args.mkString(" ").! match {
+        case 0 => state
+        case _ => state.fail
+      }
   }
 
 }
